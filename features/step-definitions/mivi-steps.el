d26810850a6f7f49ace227cb6f97182140c31e75
Iku Iwasa <iku.iwasa@gmail.com>
2016-10-09 14:04:45 +0900

    Use "should" assertion instead of "cl-assert"

--- features/step-definitions/mivi-steps.el
+++ features/step-definitions/mivi-steps.el
@@ -12,9 +12,5 @@
 (Then "^the cursor should be at cell (\\([0-9]+\\), *\\([0-9]+\\))$"
   "Checks that the cursor is at a specific (line, column)."
   (lambda (line column)
-    (let ((message "Expected cursor to be at cell '(%s, %s)', but was at '(%s, %s)'"))
-      (cl-assert (and (= (string-to-number line) (line-number-at-pos))
-                      (= (string-to-number column) (current-column)))
-                 nil
-                 message
-                 line column (line-number-at-pos) (current-column)))))
+    (should (and (= (string-to-number line) (line-number-at-pos))
+                 (= (string-to-number column) (current-column))))))
