2d8949eddc6d3956b2d0f8b39183187e07c7d77e
Iku Iwasa <iku.iwasa@gmail.com>
2018-01-25 08:35:58 +0900

    Remove cl-lib from Package-Requires
    as package-list suggests.

--- mivi.el
+++ mivi.el
@@ -5,7 +5,7 @@
 ;; Author:    Iku Iwasa <iku.iwasa@gmail.com>
 ;; URL:       https://github.com/iquiw/mivi
 ;; Version:   0.0.0
-;; Package-Requires: ((cl-lib "1.0") (undo-tree "0.6.5") (emacs "25"))
+;; Package-Requires: ((undo-tree "0.6.5") (emacs "25"))
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
